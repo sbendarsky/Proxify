44794e7aa233095e729b6fd3fc3edf8342e335f4
itay-elb <itayelbachr@gmail.com>
2024-03-28 10:13:20 +0200

    added logs feature

---
+++ pages/api/proxmox/logs.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+import https from 'https';
+import { serverRuntimeConfig } from 'next.config';
+
+const proxmox_url = serverRuntimeConfig.proxmoxURL;
+const proxmox_token_id = serverRuntimeConfig.proxmox_token_id;
+const proxmox_secret = serverRuntimeConfig.proxmox_secret;
+
+const client = axios.create({
+  baseURL: `${proxmox_url}/api2/json`,
+  headers: {
+    Authorization: `PVEAPIToken=${proxmox_token_id}=${proxmox_secret}`,
+  },
+  httpsAgent: new https.Agent({ rejectUnauthorized: false }),
+});
+
+export default async (req, res) => {
+  try {
+    const response = await client.get('/cluster/log');
+    console.log("this is the logs");
+    console.log(response.data); // Log the response data
+    res.status(200).json(response.data);
+  } catch (error) {
+    console.error(error); // Log the error
+    res.status(500).json({ error: error.toString() });
+  }
+};
