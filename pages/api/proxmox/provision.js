3656147a95929aa5b9eaf088140c00d0adee90f7
Maxim weiss <137171905+maximweiss57@users.noreply.github.com>
2024-03-28 16:27:29 +0200

    Update provision.js to clone VM instead of creating a new one

--- pages/api/proxmox/provision.js
+++ pages/api/proxmox/provision.js
@@ -7,29 +7,28 @@ const proxmox_token_id = serverRuntimeConfig.proxmox_token_id;
 const proxmox_secret = serverRuntimeConfig.proxmox_secret;
 
 const client = axios.create({
-  baseURL: `${proxmox_url}/api2/json`,
-  headers: {
-    Authorization: `PVEAPIToken=${proxmox_token_id}@pam=${proxmox_secret}`,
-  },
-  httpsAgent: new https.Agent({ rejectUnauthorized: false }),
-});
+    baseURL: `${proxmox_url}/api2/json`,
+    headers: {
+      Authorization: `PVEAPIToken=${proxmox_token_id}=${proxmox_secret}`,
+    },
+    httpsAgent: new https.Agent({ rejectUnauthorized: false }),
+  });
 
 export default async (req, res) => {
   try {
     const node = 'proxmox24';
-    const vmid = '101';
     const vmConfig = {
-      vmid,
-      ostype: 'l26',
-      disk: '10',
-      cores: '2',
-      memory: '2048',
-      storage: 'local',
-      net0: 'virtio,bridge=vmbr159',
-      ide2: 'local:iso/Fedora-Workstation-Live-x86_64-39-1.5.iso', // Specify the ISO image correctly
+        target: node,
+        node: node, 
+        vmid: 183,
+        newid: 195,
+        pool: 'Ort',
     };
 
-    const response = await client.post(`/nodes/${node}/qemu/`, vmConfig);
+    // Access the vmid from vmConfig
+    const vmid = vmConfig.vmid;
+
+    const response = await client.post(`/nodes/${node}/qemu/${vmid}/clone`, vmConfig);
 
     if (response.status === 200) {
       res.json({ message: 'VM created successfully' });
