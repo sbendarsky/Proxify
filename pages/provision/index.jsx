9a2e2b8cd8d513695d8b2b86c9f5c72226ce209b
Stav Bendarsky <sbendarsky@gmail.com>
2024-03-29 22:44:26 +0300

    improved ui

--- pages/provision/index.jsx
+++ pages/provision/index.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { Layout } from 'components/users'; // Assuming you have a similar Layout component for VMs
 
 const ProvisionPage = () => {
     const [vmid, setVmid] = useState('');
@@ -29,59 +30,38 @@ const ProvisionPage = () => {
     };
 
     return (
-        <div className="centered-container"> {/* Apply centered-container class */}
-            <div>
-                <label htmlFor="vmid">VMID:</label>
-                <input
-                    type="text"
-                    id="vmid"
-                    value={vmid}
-                    onChange={(e) => setVmid(e.target.value)}
-                    placeholder="Enter VMID"
-                />
-            </div>
-            <div>
-                <label htmlFor="name">Name:</label>
-                <input
-                    type="text"
-                    id="name"
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}
-                    placeholder="Enter VM Name"
-                />
-            </div>
-            <div>
-                <label htmlFor="memory">Memory (MB):</label>
-                <input
-                    type="text"
-                    id="memory"
-                    value={memory}
-                    onChange={(e) => setMemory(e.target.value)}
-                    placeholder="4096"
-                />
-            </div>
-            <div>
-                <label htmlFor="cores">Cores:</label>
-                <input
-                    type="text"
-                    id="cores"
-                    value={cores}
-                    onChange={(e) => setCores(e.target.value)}
-                    placeholder="2"
-                />
-            </div>
-            <div>
-                <label htmlFor="os">OS:</label>
-                <input
-                    type="text"
-                    id="os"
-                    value={os}
-                    onChange={(e) => setOS(e.target.value)}
-                    placeholder="local:iso/alpine-standard-3.19.1-x86.iso,media=cdrom"
-                />
-            </div>
-            <button onClick={provisionVM}>Provision VM</button>
-        </div>
+        <Layout>
+            <h1>Provision New Virtual Machine</h1>
+            <form>
+                <table className="table table-striped">
+                    <tbody>
+                        <tr>
+                            <td><label htmlFor="vmid">VMID:</label></td>
+                            <td><input type="text" id="vmid" value={vmid} onChange={(e) => setVmid(e.target.value)} className="form-control" placeholder="Enter VMID" /></td>
+                        </tr>
+                        <tr>
+                            <td><label htmlFor="name">Name:</label></td>
+                            <td><input type="text" id="name" value={name} onChange={(e) => setName(e.target.value)} className="form-control" placeholder="Enter VM Name" /></td>
+                        </tr>
+                        <tr>
+                            <td><label htmlFor="memory">Memory (MB):</label></td>
+                            <td><input type="text" id="memory" value={memory} onChange={(e) => setMemory(e.target.value)} className="form-control" placeholder="4096" /></td>
+                        </tr>
+                        <tr>
+                            <td><label htmlFor="cores">Cores:</label></td>
+                            <td><input type="text" id="cores" value={cores} onChange={(e) => setCores(e.target.value)} className="form-control" placeholder="2" /></td>
+                        </tr>
+                        <tr>
+                            <td><label htmlFor="os">OS:</label></td>
+                            <td><input type="text" id="os" value={os} onChange={(e) => setOS(e.target.value)} className="form-control" placeholder="local:iso/alpine-standard-3.19.1-x86.iso,media=cdrom" /></td>
+                        </tr>
+                    </tbody>
+                </table>
+                <div className="text-center">
+                    <button type="button" onClick={provisionVM} className="btn btn-primary">Provision VM</button>
+                </div>
+            </form>
+        </Layout>
     );
 };
 
